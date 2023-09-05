1734c28fd9829c42ac1ea4b63b9ecd21ee9e2150
mmustafoyev <mmustafoyev123@gmail.com>
2023-09-05 23:19:22 +0500

    get stright

--- task2-singleton-pattern/src/main/java/com/mjc/stage2/ThreadSafeSingleton.java
+++ task2-singleton-pattern/src/main/java/com/mjc/stage2/ThreadSafeSingleton.java
@@ -6,15 +6,18 @@ import java.util.concurrent.locks.ReentrantLock;
 public class ThreadSafeSingleton {
     // Write your code here!
     private static AtomicReference<ThreadSafeSingleton> instance = new AtomicReference<>();
+    private static final ReentrantLock lock = new ReentrantLock();
+
     private ThreadSafeSingleton() {
     }
 
     public static ThreadSafeSingleton getInstance() {
+        lock.lock();
         if (instance == null || instance.get() == null){
             instance = new AtomicReference<>(new ThreadSafeSingleton());
         }
         var result = instance.get();
-
+        lock.unlock();
         return result;
     }
 
